f68c8a569b3733711b3b5727688afecf2fdc9264
Yannis LABIDI <yannis.labibi666@gmail.com>
2020-05-28 13:51:58 +0200

    setup routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MedicShopComponent } from './medic-shop/medic-shop.component';
-import { AuthComponent } from './auth/auth.component';
 import { MedicViewComponent } from './medic-view/medic-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MedicShopComponent,
-    AuthComponent,
     MedicViewComponent
   ],
   imports: [
